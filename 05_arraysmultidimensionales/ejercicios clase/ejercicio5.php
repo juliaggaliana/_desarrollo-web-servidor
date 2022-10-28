9a361da1d00bcf284cceb1d4daed245a24ce6209
Julia G Galiana <bluejutebook@gmail.com>
2022-10-28 08:57:24 +0200

    primer commit del dia

--- 05_arraysmultidimensionales/ejercicios clase/ejercicio5.php
+++ 05_arraysmultidimensionales/ejercicios clase/ejercicio5.php
@@ -7,6 +7,45 @@
     <title>Ejercicio 5</title>
 </head>
 <body>
+     <h1>Ejercicio 5</h1>
+
+
+     <?php
+     $estudiantes = [
+        ["Luis", rand(0,10)]
+        ["Alfredo", rand(0,10)]
+        ["Elena", rand(0,10)]
+
+     ];
+     ?>
+
+
+
+<table>
+            <tr>
+                <th>Nombre</th>
+                <th>Nota</th>
+                <th>Calificacion</th>
+            </tr>
+            <?php
+            foreach ($estudiantes as $estudiantre){
+                list($nombre, $nota) = $estudiante;
+            ?>
+
+<tr>
+                <td>Nombre</td>
+                <td>Nota</td>
+                
+            </tr>
+
+
+
+
+
+            }
+
+
+ </table>            
     
 </body>
 </html>
